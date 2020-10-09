3b730d2f11d81ed3b252bb416b285e7e059b42fc
Hans <hansloesche.art@gmail.com>
2020-10-09 18:49:30 -0400

    new branch opengl_texture

--- OpenGLTutorial/assets/fragment_core.glsl
+++ OpenGLTutorial/assets/fragment_core.glsl
@@ -3,8 +3,13 @@ out vec4 FragColor;
 
 //uniform vec4 ourColor;
 in vec3 ourColor;
+in vec2 TexCoord;
 
-void main()
-{
-    FragColor = vec4(ourColor, 1.0f);
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main(){
+   // FragColor = vec4(ourColor, 1.0f);
+   // FragColor = vec4(ourColor, 1.0f) * texture(texture1, TexCoord);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), .65);
 } 
