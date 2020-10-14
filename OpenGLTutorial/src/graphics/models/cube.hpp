d1bc069dc274878a543b54622a0b8888d21924d5
Hans <hansloesche.art@gmail.com>
2020-10-14 11:15:45 -0400

    updated

--- OpenGLTutorial/src/graphics/models/cube.hpp
+++ OpenGLTutorial/src/graphics/models/cube.hpp
@@ -6,15 +6,15 @@
 
 class Cube : public Model {
 public:
-	glm::vec3 pos;
-	glm::vec3 size;
+    glm::vec3 pos;
+    glm::vec3 size;
 
 
-	Cube( glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f))
-		: pos(pos), size(size) {}
+    Cube(glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 size = glm::vec3(1.0f, 1.0f, 1.0f))
+        : pos(pos), size(size) {}
 
-	void init() {
-		int noVertices = 36;
+    void init() {
+        int noVertices = 36;
 
         float vertices[] = {
             // position                 normal              texcoord
@@ -61,10 +61,10 @@ public:
             -0.5f,  0.5f, -0.5f,     0.0f,  1.0f,  0.0f,    0.0f, 1.0f
         };
 
-		std::vector<unsigned int> indices(noVertices);
-		for (unsigned int i = 0; i < noVertices; i++) {
-			indices[i] = i;
-		}
+        std::vector<unsigned int> indices(noVertices);
+        for (unsigned int i = 0; i < noVertices; i++) {
+            indices[i] = i;
+        }
         Texture tex("assets/flag.png", "material.diffuse");
         tex.load();
 
@@ -72,23 +72,23 @@ public:
         tex_specular.load();
 
         mesh = Mesh(Vertex::genList(vertices, noVertices), indices, { tex, tex_specular });
-	}
+    }
 
-	void render(Shader shader) {
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, pos);
-		model = glm::scale(model, size);
-		//model = glm::rotate(model, (float)glfwGetTime() * glm::radians(-55.0f), glm::vec3(0.5f, 0.5f, 0.5f));
-		shader.setMat4("model", model);
+    void render(Shader shader) {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, pos);
+        model = glm::scale(model, size);
+        //model = glm::rotate(model, (float)glfwGetTime() * glm::radians(-55.0f), glm::vec3(0.5f, 0.5f, 0.5f));
+        shader.setMat4("model", model);
 
-        
+
         //shader.setInt("material.diffuse", mesh.textures[0].id);
         mesh.textures[0].activate();
         mesh.textures[1].activate();
         shader.setFloat("material.shininess", 0.5f);
 
-		Model::render(shader);
-	}
+        Model::render(shader);
+    }
 };
 
 #endif
